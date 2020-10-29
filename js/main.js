982c20da9c5cf5af84578fcd3195423ed951f2db
Rahana Islam <68787028+rahanaislam99@users.noreply.github.com>
2020-10-29 21:18:06 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+$('.slider-active').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    slideSpeed : 5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+
+
+
+
+})
